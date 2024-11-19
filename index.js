3c9f3797664d32892b060d16b92871bfdead25c2
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2024-11-19 14:43:00 +0100

    Function och loop är gjord för #cities (HTML)
    Just nu har jag skapat en function som innehåller:
    - Skapad div
    -Class som skapas och läggs på div "cityButton"
    - #cities är hämtad och appendChild med div "cityButton".
    
    Loop:
    - Skapat en loop med cities.length för att få fram hela längden på arrayen

--- index.js
+++ index.js
@@ -4,3 +4,14 @@
 
 // Recommended: Ask for the city name and then the rest of the code
 
+
+/*------------------nameOfCity-------------------*/
+function createBox (nameOfCity) {
+    const cityButton = document.createElement("div");
+    cityButton.classList.add("cityBox");
+    cityButton.textContent = nameOfCity;
+    document.getElementById("cities").appendChild(cityButton);
+}
+for (let i = 0; i < cities.length; i++){
+    createBox(cities[i].name);
+}
