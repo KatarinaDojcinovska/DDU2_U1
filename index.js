76adb7a1cb438d9a807cadefa2a1a8dac2fb65af
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2024-11-20 12:06:04 +0100

    Första försöket till jämföra distansen mellan city1 och city2 med id från arrayen "cities"
    Detta är bara ett första försök som senare kan ändras.

--- index.js
+++ index.js
@@ -30,6 +30,30 @@ function createBox (nameOfCity) {
 for (let i = 0; i < cities.length; i++){
     createBox(cities[i].name);
 }
+
+/*---------------------Prompt-------------------------*/
+
+for (let i = 0; i < cities.length; i++){
+    let city = cities[i];
+    let city_name = city.name;
+
+    if (user_request_city === city_name){
+        document.querySelector("h2").textContent = city_name;
+        console.log(true);
+        rate = true;
+    }
+}
+    if (rate == false) {
+        document.querySelector("h2").textContent = `${user_request_city} is not in the database`;
+        console.log(false);
+}
+
+/*-------------------Further and Closest-------------------*/
+
+    function distance (){
+        let closest_city = [];
+        for (let i = 0; i < distances.length; i++){
+        if (distances[i].city1 == user_request_city.id || distances[i].city2 == user_request_city.id ) {
             closest_city.push(distance[i]);
             console.log(city1, city2);
         }
