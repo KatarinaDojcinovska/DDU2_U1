4a2bf04fd7188e6f5b682b1fd4579a79836de6f6
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2024-11-20 12:03:33 +0100

    Skapat title för "not found" och "(stad)"
    Här skapar jag en if om prompten är true och staden finns står staden i fliken.
    Finns den inte så står det "Not found"

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ document.getElementById("cities");
 
 // Recommended: Ask for the city name and then the rest of the code
 
+const user_request_city = prompt("Vilken stad?");
+let rate = false;
 
 /*------------------nameOfCity-------------------*/
 function createBox (nameOfCity) {
@@ -15,7 +17,16 @@ function createBox (nameOfCity) {
     cityButton.classList.add("cityBox");
     cityButton.textContent = nameOfCity;
     document.getElementById("cities").appendChild(cityButton);
+
+    if (nameOfCity.includes(user_request_city)){
+        title.textContent = user_request_city;
+        rate = true;
+    } 
 }
+     if (rate == false) { 
+        title.textContent = "Not found";
+    }
+    
 for (let i = 0; i < cities.length; i++){
     createBox(cities[i].name);
 }
