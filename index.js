87e812a69bc87006085877d6bb301b1183d057e0
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2024-11-20 12:01:31 +0100

    Början på att plocka in från HTML och skapar querySelector och getElementById

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 // Recommended: constants with references to existing HTML-elements
 
+let title = document.querySelector("title");
+document.getElementById("cities");
+
+
 // Recommended: Ask for the city name and then the rest of the code
 
 
@@ -15,3 +19,19 @@ function createBox (nameOfCity) {
 for (let i = 0; i < cities.length; i++){
     createBox(cities[i].name);
 }
+            closest_city.push(distance[i]);
+            console.log(city1, city2);
+        }
+    }
+    return closest_city;
+}
+   distance(longest_city);
+
+   function longest_city (longest){
+    for(let i= 0; i < longest.length; i++){ //kolla vilket värde som är störst i arrayen
+    }
+}
+
+
+
+
